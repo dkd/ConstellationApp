f19d72680b2807ef193458e33387a8bdecfff63d
Stefan Sprenger <stefan.sprenger@dkd.de>
2010-10-08 17:14:00 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://rubygems.org'
+
+# Rails
+gem 'rails', '3.0.0'
+
+# Database
+gem 'mysql'
+
+# Deployment
+gem 'capistrano'
+
+# Development and testing
+group :development, :test do
+  gem 'ruby-debug'
+  gem 'rspec', '2.0.0.rc'
+  gem 'rspec-rails', '2.0.0.rc'
+  gem 'webrat'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Constellation::Application.config.secret_token = '1917e72271535d996fe10ddf6c524e0ea01f45159ae24b56b87cecaec7a94cc4b8d1567cbafaa5aebc852a8f38926f917154bcf0f02abfecef83e996d2881f0f'
