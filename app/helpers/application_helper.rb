2b51b4255b4ece348db4068ef6c0bfbb15c44537
Stefan Sprenger <stefan.sprenger@dkd.de>
2010-10-12 16:47:37 +0200

    Show user state-specific javascripts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # return the correct javascript files depending on if the user is logged in or logged out
+  def include_javascript
+    if user_signed_in?
+      javascript_include_tag("ext/adapter/ext/ext-base", "ext/ext-all", "GroupTab",
+                              "GroupTabPanel", "TabCloseMenu", "constellation",
+                              "application", :cache => true)
+    else
+      javascript_include_tag("ext/adapter/ext/ext-base", "ext/ext-all", :cache => "login")
+    end
+  end
 end
