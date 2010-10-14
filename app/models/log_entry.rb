d955b8ca7f39f372ede4abdf74cd76cbe59b2195
Stefan Sprenger <stefan.sprenger@dkd.de>
2010-10-14 17:13:46 +0200

    Added some common methods for saving and retrieving log entries to and from Cassandra

--- app/models/log_entry.rb
+++ app/models/log_entry.rb
@@ -1,2 +1,71 @@
-class LogEntry
+#
+# Represents a log entry having the following attributes:
+#
+# * uuid (key)
+# * machine
+# * application
+# * message
+# * timestamp
+#
+class LogEntry < Constellation::LogEntry
+
+  @@data_store = Constellation::DataStore.instance
+
+  #
+  # Initializes and parses the given attributes.
+  #
+  def initialize(attributes = {})
+    @uuid         = attributes["uuid"]
+    @machine      = attributes["machine"]
+    @application  = attributes["application"]
+    @timestamp    = attributes["timestamp"]
+    @message      = attributes["message"]
+    self
+  end
+
+  #
+  # Saves the log entry and returns itself.
+  #
+  def save
+    @@data_store.insert(self)
+    self
+  end
+
+  class << self
+
+    #
+    # Returns all log entries found in the Cassandra data store.
+    #
+    # You can limit the results by defining options.
+    #
+    # E.g. if you want to get only 50 entries, you can do this by:
+    #
+    #   LogEntry.all(:count => 50)
+    #
+    # == Available options
+    #
+    # * start
+    # * finish
+    # * consistency
+    # * count
+    #
+    def all(options = {})
+      results      = []
+      @@data_store.get_range(options).each { |h|
+        attributes = {}
+        h.columns.each { |c| attributes[c.column.name] = c.column.value }
+        results   << LogEntry.new(attributes)
+      }
+      results
+    end
+
+    #
+    # Returns the log entry defined by the given uuid
+    #
+    def get(uuid)
+      LogEntry.new(@@data_store.get(uuid))
+    end
+
+  end
+
 end
--- spec/models/log_entry_spec.rb
+++ spec/models/log_entry_spec.rb
@@ -1,4 +1,30 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe LogEntry do
+  before(:each) do
+    @log_entry = LogEntry.new
+  end
+
+  describe "#save" do
+    it "should insert itself into the data store" do
+      LogEntry.__send__("class_variable_get", "@@data_store").should_receive(:insert)
+      @log_entry.save
+    end
+  end
+
+  describe ".all" do
+    it "should call the data store" do
+      LogEntry.stub!(:new)
+      LogEntry.__send__("class_variable_get", "@@data_store").should_receive(:get_range).and_return([])
+      LogEntry.all
+    end
+  end
+
+  describe ".get" do
+    it "should call the data store" do
+      LogEntry.stub!(:new)
+      LogEntry.__send__("class_variable_get", "@@data_store").should_receive(:get)
+      LogEntry.get('123abc-321cd')
+    end
+  end
 end
