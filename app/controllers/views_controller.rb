ecbe239c61ed1ace7e026d7baed3549f07947c11
Stefan Sprenger <stefan.sprenger@dkd.de>
2010-10-15 11:24:42 +0200

    Added comments

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -1,6 +1,8 @@
 class ViewsController < ApplicationController
   before_filter :authenticate_user!
 
+  # GET /views
+  #----------------------------------------------------------------------------
   def index
   end
 end
