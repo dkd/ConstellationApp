1f868db85933e7f7e46aa23834775f281c783dfb
Stefan Sprenger <stefan.sprenger@dkd.de>
2010-10-18 10:21:02 +0200

    Don't establish a connection to Cassandra for tests

--- config/initializers/constellation.rb
+++ config/initializers/constellation.rb
@@ -1,11 +1,14 @@
-raise Constellation::ConstellationFileNotFoundError unless File.exists?("ConstellationFile")
-config = Constellation::Config.instance
+# Don't run this initializer for tests
+unless ENV["RAILS_ENV"]=="test"
+  raise Constellation::ConstellationFileNotFoundError unless File.exists?("ConstellationFile")
+  config = Constellation::Config.instance
 
-# parse ConstellationFile
-begin
-  config.instance_eval(File.read("ConstellationFile"))
-rescue SyntaxError
-  raise Constellation::InvalidConstellationFileError
+  # parse ConstellationFile
+  begin
+    config.instance_eval(File.read("ConstellationFile"))
+  rescue SyntaxError
+    raise Constellation::InvalidConstellationFileError
+  end
+
+  config.data_store.establish_connection
 end
-
-config.data_store.establish_connection
