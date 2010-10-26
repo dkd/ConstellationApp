b9cc34c26ca9b1d64a6e7d4eb5753b59272a2905
Stefan Sprenger <stefan.sprenger@dkd.de>
2010-10-26 15:33:01 +0200

    Introducing LogEntry#all for providing log entries ordered by application or machine

--- app/models/log_entry.rb
+++ app/models/log_entry.rb
@@ -8,7 +8,6 @@
 # * timestamp
 #
 class LogEntry < Constellation::LogEntry
-
   @@data_store = Constellation::DataStore.instance
 
   #
@@ -32,7 +31,6 @@ class LogEntry < Constellation::LogEntry
   end
 
   class << self
-
     #
     # Returns all log entries found in the Cassandra data store.
     #
@@ -40,7 +38,7 @@ class LogEntry < Constellation::LogEntry
     #
     # E.g. if you want to get only 50 entries, you can do this by:
     #
-    #   LogEntry.all(:count => 50)
+    #   LogEntry.current_epoch(:count => 50)
     #
     # == Available options
     #
@@ -49,13 +47,48 @@ class LogEntry < Constellation::LogEntry
     # * consistency
     # * count
     #
-    def current_epoch(options = {})
+    def current_epoch(options={})
       results       = []
       current_time  = Time.now
-      @@data_store.get("#{current_time.year}/#{current_time.month}/#{current_time.day}/#{current_time.hour}", options).each { |log_entry|
-        attributes          = log_entry[1]
-        attributes["uuid"]  = log_entry[0].to_guid
+      @@data_store.get(:logs, "#{current_time.year}/#{current_time.month}/#{current_time.day}/#{current_time.hour}", options).each { |log_entry|
+        attributes         = log_entry[1]
+        attributes["uuid"] = log_entry[0].to_guid
+        results << new(attributes)
+      }
+      results
+    end
 
+    #
+    # Provides an ordered list of log entries.
+    #
+    # You can define the attribute by which log entries should be ordered by using :order => "attribute_name"
+    #
+    # E.g. if you want log entries ordered by their application, you can do this by:
+    #
+    #   LogEntry.all(:order => "application")
+    #
+    # == Furthermore the following options are available:
+    #
+    # * consistency
+    # * count
+    #
+    def all(options={})
+      case options[:order]
+      when "application"
+        column_family = :logs_by_application
+      when "machine"
+        column_family = :logs_by_machine
+      else
+        return current_epoch(options)
+      end
+      options.delete(:order)
+      results       = []
+      current_time  = Time.now
+      key           = "#{current_time.year}/#{current_time.month}/#{current_time.day}/#{current_time.hour}"
+      @@data_store.get(column_family, key, options).each { |log_entry|
+        uuid               = log_entry[1].keys.first.to_guid
+        attributes         = @@data_store.get(:logs, key, uuid)
+        attributes["uuid"] = uuid
         results << new(attributes)
       }
       results
@@ -67,7 +100,5 @@ class LogEntry < Constellation::LogEntry
     def get(uuid)
       new(@@data_store.get(uuid))
     end
-
   end
-
 end
--- spec/models/log_entry_spec.rb
+++ spec/models/log_entry_spec.rb
@@ -20,6 +20,33 @@ describe LogEntry do
     end
   end
 
+  describe ".all" do
+    before(:each) do
+      @data_store  = Constellation::DataStore.instance
+      current_time = Time.now
+      @key         = "#{current_time.year}/#{current_time.month}/#{current_time.day}/#{current_time.hour}"
+    end
+
+    context "given order by application" do
+      it "should use the logs_by_application column family" do
+        @data_store.should_receive(:get).with(:logs_by_application, @key, anything).and_return([])
+        LogEntry.all(:order => "application")
+      end
+    end
+    context "given order by machine" do
+      it "should use the logs_by_machine column family" do
+        @data_store.should_receive(:get).with(:logs_by_machine, @key, anything).and_return([])
+        LogEntry.all(:order => "machine")
+      end
+    end
+    context "given no order" do
+      it "should call .current_epoch" do
+        LogEntry.should_receive(:current_epoch)
+        LogEntry.all
+      end
+    end
+  end
+
   describe ".get" do
     it "should call the data store" do
       LogEntry.stub!(:new)
