c19f21e3c8ccc393a4b088844c7559f8dd4d82f4
Stefan Sprenger <stefan.sprenger@dkd.de>
2010-11-01 13:57:46 +0100

    Added test environment

--- config/initializers/constellation.rb
+++ config/initializers/constellation.rb
@@ -1,14 +1,11 @@
 # Run this initializer only in development or production environment
-if %w(development production).include? Rails.env
-  raise Constellation::ConstellationFileNotFoundError unless File.exists?("ConstellationFile")
-  config = Constellation::Config.instance
+raise Constellation::ConstellationFileNotFoundError unless File.exists?("ConstellationFile")
+config = Constellation::Config.instance
 
-  # parse ConstellationFile
-  begin
-    config.instance_eval(File.read("ConstellationFile"))
-  rescue SyntaxError
-    raise Constellation::InvalidConstellationFileError
-  end
-
-  config.data_store.establish_connection
+# parse ConstellationFile
+begin
+  config.instance_eval(File.read("ConstellationFile"))
+rescue SyntaxError
+  raise Constellation::InvalidConstellationFileError
 end
+config.data_store.establish_connection
