cb38179f8aa4c16d284151d03e80ec3aeed200a9
Stefan Sprenger <stefan.sprenger@dkd.de>
2010-11-02 11:08:59 +0100

    Fixed specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,14 +7,23 @@ describe UsersController do
     LogEntry.stub!(:current_epoch)
     @user = Factory.create(:user)
     sign_in :user, @user
+    @ar_relation = mock(ActiveRecord::Relation)
+    @ar_relation.stub!(:find)
+    User.stub!(:select).and_return(@ar_relation)
   end
 
   describe "GET /users.json" do
     it_should_be_protected :index, :format => :json
 
+    it "should render only id and email attributes" do
+      controller.stub!(:authenticate_user!)
+      User.should_receive(:select).with(:id, :email)
+      get :index, :format => :json
+    end
+
     it "should render all users" do
       controller.stub!(:authenticate_user!)
-      User.should_receive(:find).with(:all)
+      @ar_relation.should_receive(:find).with(:all)
       get :index, :format => :json
     end
   end
