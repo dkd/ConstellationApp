7abc6efb96d4519279d9a5cef7a64c2f8ff6a7cb
Stefan Sprenger <stefan.sprenger@dkd.de>
2010-11-05 13:17:53 +0100

    Removed validation

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -1,7 +1,3 @@
 class Filter < ActiveRecord::Base
   belongs_to :view
-
-  validates_presence_of   :property,   :query_type
-  validates_inclusion_of  :property,   :in => %(date machine application)
-  validates_inclusion_of  :query_type, :in => %(compare range)
 end
